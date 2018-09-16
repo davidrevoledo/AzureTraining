d23e62f7dfabef89742aa8bfa07c9866fe7e9276
David Revoledo <davidrevoledo@d-genix.com>
2018-09-16 13:31:29 -0300

    Invoice v1 (Long work in the api side)

---
+++ InvoiceV1/Controllers/InvoiceController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceV1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Get()
+        {
+            var invoiceNumber = await GenerateInvoice();
+
+            return Ok($"Your Invoice has been generated {invoiceNumber}");
+        }
+
+        private async Task<string> GenerateInvoice()
+        {
+            var task = Task.Delay(10000);
+
+            await task;
+
+            return Guid.NewGuid().ToString("N");
+        }
+    }
+}
