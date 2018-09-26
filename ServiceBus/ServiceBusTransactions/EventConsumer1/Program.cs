20b74fc23e29b8484246863a1fdb30b1297f196d
David Revoledo <davidrevoledo@d-genix.com>
2018-09-25 23:12:13 -0300

    Service bus transaction sample

---
+++ ServiceBus/ServiceBusTransactions/EventConsumer1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+
+namespace EventConsumer1
+{
+    internal class Program
+    {
+        private const string connectionString =
+            @"Endpoint=sb://servicebusazure134.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Pp7n9VNZak7DEoGZ8CWjV0UCl9iKilrqhZ0Sncd7Zec=";
+
+        private const string queueName = "transactionQueue";
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Consumer ready ! ");
+
+            var client = new QueueClient(connectionString, queueName);
+
+            // Configure the message handler options in terms of exception handling, number of concurrent messages to deliver, etc.
+            var messageHandlerOptions = new MessageHandlerOptions(LogMessageHandlerException)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = true
+            };
+
+            client.RegisterMessageHandler((message, token) =>
+            {
+                Console.WriteLine($"message received ! {message.MessageId}");
+                return Task.CompletedTask;
+
+            }, messageHandlerOptions);
+
+            Console.ReadLine();
+        }
+
+        private static Task LogMessageHandlerException(ExceptionReceivedEventArgs e)
+        {
+            Console.WriteLine("Exception: \"{0}\" {1}", e.Exception.Message, e.ExceptionReceivedContext.EntityPath);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ServiceBus/ServiceBusTransactions/EventConsumer2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+
+namespace EventConsumer2
+{
+    internal class Program
+    {
+        private const string connectionString =
+            @"Endpoint=sb://servicebusazure134.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Pp7n9VNZak7DEoGZ8CWjV0UCl9iKilrqhZ0Sncd7Zec=";
+
+        private const string queueName = "sessionqueue";
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Consumer #2");
+
+            var client = new QueueClient(connectionString, queueName);
+            client.RegisterSessionHandler(
+                async (session, message, cancellationToken) =>
+                {
+                    if (message.Label != null &&
+                        message.ContentType != null &&
+                        message.Label.Equals("RecipeStep", StringComparison.InvariantCultureIgnoreCase) &&
+                        message.ContentType.Equals("application/json", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        var body = message.Body;
+
+                        dynamic recipeStep = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(body));
+                        lock (Console.Out)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine(
+                                "\t\t\t\tMessage received:  \n\t\t\t\t\t\tSessionId = {0}, \n\t\t\t\t\t\tMessageId = {1}, \n\t\t\t\t\t\tSequenceNumber = {2}," +
+                                "\n\t\t\t\t\t\tContent: [ step = {3}, title = {4} ]",
+                                message.SessionId,
+                                message.MessageId,
+                                message.SystemProperties.SequenceNumber,
+                                recipeStep.step,
+                                recipeStep.title);
+                            Console.ResetColor();
+                        }
+
+                        await session.CompleteAsync(message.SystemProperties.LockToken);
+
+                        //if (recipeStep.step == 5) await session.CloseAsync();
+                    }
+                    else
+                    {
+                        await session.DeadLetterAsync(message.SystemProperties
+                            .LockToken); //, "BadMessage", "Unexpected message");
+                    }
+                },
+                new SessionHandlerOptions(LogMessageHandlerException)
+                {
+                    MessageWaitTimeout = TimeSpan.FromSeconds(5),
+                    MaxConcurrentSessions = 1,
+                    AutoComplete = false
+                });
+
+            Console.ReadLine();
+        }
+
+        private static Task LogMessageHandlerException(ExceptionReceivedEventArgs e)
+        {
+            Console.WriteLine("Exception: \"{0}\" {1}", e.Exception.Message, e.ExceptionReceivedContext.EntityPath);
+            return Task.CompletedTask;
+        }
+    }
+}
