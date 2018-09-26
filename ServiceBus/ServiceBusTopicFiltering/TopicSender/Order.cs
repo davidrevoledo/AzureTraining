2d4af9f68384b8ad8f6db85cdb207f3838efbbd9
David Revoledo <davidrevoledo@d-genix.com>
2018-09-26 08:19:19 -0300

    Filtering azure service bus

---
+++ ServiceBus/ServiceBusTopicFiltering/TopicSender/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopicSender
+{
+    class Order
+    {
+        public string Color
+        {
+            get;
+            set;
+        }
+
+        public int Quantity
+        {
+            get;
+            set;
+        }
+
+        public string Priority
+        {
+            get;
+            set;
+        }
+    }
+}
