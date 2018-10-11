00e0fb00b99d5b8412b5772b0dacd31bd92da6f6
David Revoledo <davidrevoledo@d-genix.com>
2018-10-11 14:36:22 -0300

    Blobs

---
+++ src/AzureStorage.Blobs/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureStorage.Blobs
+{
+    internal class Program
+    {
+        private const string StorageConnection = "";
+
+        private static void Main(string[] args)
+        {
+            MainAsync()
+                .GetAwaiter()
+                .GetResult();
+
+            Console.ReadLine();
+        }
+
+        private static async Task MainAsync()
+        {
+            await UploadFile();
+
+            //await ReadFile();
+        }
+
+        private static async Task ReadFile()
+        {
+            var containerName = "images";
+
+            CloudStorageAccount cloudStorageAccount 
+                = new CloudStorageAccount(new StorageCredentials(StorageConnection), true);
+
+            CloudBlobClient cloudBlobClient 
+                = cloudStorageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer cloudBlobContainer 
+                = cloudBlobClient.GetContainerReference(containerName);
+
+            CloudBlockBlob blockBlob =
+                cloudBlobContainer.GetBlockBlobReference("uploadedfilename.png");
+
+
+            MemoryStream memStream = new MemoryStream();
+
+            await blockBlob.DownloadToStreamAsync(memStream);
+
+            Console.WriteLine(memStream.Length);
+        }
+
+        private static async Task UploadFile()
+        {
+
+            const string cs =
+                "DefaultEndpointsProtocol=https;AccountName=storage12345david;AccountKey=Hug7WhDLbYl1dojX4OABsxteoAIjtMsi9B92W2QBIFuM+iQsZsuKrzedQC04vSFDx3WcTVB/oMFOvexTA9lwig==;EndpointSuffix=core.windows.net";
+
+            var credentials = new StorageCredentials("storage12345david",
+                @"Hug7WhDLbYl1dojX4OABsxteoAIjtMsi9B92W2QBIFuM+iQsZsuKrzedQC04vSFDx3WcTVB/oMFOvexTA9lwig==");
+
+            var cloudStorageAccount = new CloudStorageAccount(credentials, true);
+
+            //create a block blob
+            CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("images2");
+
+            //create a container if it is not already exists
+            if (await cloudBlobContainer.CreateIfNotExistsAsync())
+            {
+                //await cloudBlobContainer.SetPermissionsAsync(new BlobContainerPermissions
+                //{
+                //    PublicAccess = BlobContainerPublicAccessType.Blob
+                //});
+            }
+
+            FileStream fileStream = File.OpenRead(@"C:\Users\Deivit\Desktop\Screenshot_2.png"); // or
+
+            CloudBlockBlob cloudBlockBlob =
+                cloudBlobContainer.GetBlockBlobReference($"{Guid.NewGuid()}image.png");
+            cloudBlockBlob.Properties.ContentType = "application/octet-stream";
+            cloudBlockBlob.Metadata.Add("name", "david");
+
+            await cloudBlockBlob.UploadFromStreamAsync(fileStream);
+        }
+    }
+}
---
+++ src/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(1000);
+
+            return Ok("Tu factura se genero  1");
+        }
+
+    }
+}
