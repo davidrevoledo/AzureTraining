fc2a7ff63afe212f4e17f2802284dac8503a8376
David Revoledo <davidrevoledo@d-genix.com>
2018-10-16 14:03:02 -0300

    Queing operations

---
+++ src/AzureStorageQueue/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace AzureStorageQueue
+{
+    internal class Program
+    {
+        // Parse the connection string and return a reference to the storage account.
+        private static readonly CloudStorageAccount storageAccount = CloudStorageAccount.Parse("");
+        private static void Main(string[] args)
+        {
+            MainAsync()
+                .GetAwaiter()
+                .GetResult();
+
+            Console.ReadLine();
+        }
+
+        private static async Task MainAsync()
+        {
+            await PushMessage();
+
+            await RaedMessage();
+
+            await ChangeMessageContent();
+
+            await GetBatchMessages();
+        }
+
+        private static async Task GetBatchMessages()
+        {
+            var queueClient = storageAccount.CreateCloudQueueClient();
+
+            var queue = queueClient.GetQueueReference("myqueue");
+
+            // Fetch the queue attributes.
+            await queue.FetchAttributesAsync();
+
+            // Retrieve the cached approximate message count.
+            int? cachedMessageCount = queue.ApproximateMessageCount;
+
+            // Display number of messages.
+            Console.WriteLine("Number of messages in queue: " + cachedMessageCount);
+        }
+
+        private static async Task ChangeMessageContent()
+        {
+            var queueClient = storageAccount.CreateCloudQueueClient();
+
+            var queue = queueClient.GetQueueReference("myqueue");
+
+            // Create the queue if it doesn't already exist
+            // Get the message from the queue and update the message contents.
+            var message = await queue.GetMessageAsync();
+            message.SetMessageContent("Updated contents.");
+
+            await queue.UpdateMessageAsync(message,
+                TimeSpan.FromSeconds(60.0), // Make it invisible for another 60 seconds.
+                MessageUpdateFields.Content | MessageUpdateFields.Visibility);
+        }
+
+        private static async Task RaedMessage()
+        {
+            var queueClient = storageAccount.CreateCloudQueueClient();
+
+            var queue = queueClient.GetQueueReference("myqueue");
+
+            // Create the queue if it doesn't already exist
+            await queue.CreateIfNotExistsAsync();
+
+            var messaage = await queue.PeekMessageAsync();
+
+            var message2 = await queue.GetMessageAsync();
+
+
+            await queue.DeleteMessageAsync(messaage);
+        }
+
+        private static async Task PushMessage()
+        {
+            var queueClient = storageAccount.CreateCloudQueueClient();
+
+            var queue = queueClient.GetQueueReference("myqueue");
+
+            // Create the queue if it doesn't already exist
+            await queue.CreateIfNotExistsAsync();
+
+            await queue.AddMessageAsync(new CloudQueueMessage("hola mundo"));
+        }
+    }
+}
